6883cd36d522a77299d7ca0f49142a24c678c00f
Lee, yeeun <ye7426@naver.com>
2025-08-07 19:56:57 +0900

    Chore/#1: node page 기능 구현

---
+++ Eventory/src/main/java/com/saym/eventory/EventoryApplication.java
@@ -0,0 +1,13 @@
+package com.saym.eventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoryApplication.class, args);
+	}
+
+}
---
+++ Eventory/src/test/java/com/saym/eventory/EventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saym.eventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
