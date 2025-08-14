95b75012caf00f5318e3c995cd8d47f499801002
Lee, yeeun <ye7426@naver.com>
2025-08-15 02:54:51 +0900

    Refactor/#12: 배포 Swagger 수정

--- Eventory/src/main/java/com/saym/eventory/global/config/SwaggerConfig.java
+++ Eventory/src/main/java/com/saym/eventory/global/config/SwaggerConfig.java
@@ -40,7 +40,8 @@ public class SwaggerConfig {
 
     private List<Server> servers() {
         return List.of(new Server()
-                .url("https://book-hub.store:8080")
+                .url("https://book-hub.store")
+                //.url("http://localhost:8080")
                 .description("Configured Server")
         );
     }
